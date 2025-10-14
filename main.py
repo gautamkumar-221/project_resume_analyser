77433481b245888382859776388d39226563d143
Gautam Kumar <gautam.kumar_cs23@gla.ac.in>
2025-10-14 20:17:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,29 @@ def calculate_similarity_bert(text1, text2):
 def get_report(resume,job_desc):
     client = Groq(api_key=api_key)
 
-   
+    # Change the prompt to get the results in your style
+    prompt=f"""
+    # Context:
+    - You are an AI Resume Analyzer, you will be given Candidate's resume and Job Description of the role he is applying for.
+
+    # Instruction:
+    - Analyze candidate's resume based on the possible points that can be extracted from job description,and give your evaluation on each point with the criteria below:  
+    - Consider all points like required skills, experience,etc that are needed for the job role.
+    - Calculate the score to be given (out of 5) for every point based on evaluation at the beginning of each point with a detailed explanation.  
+    - If the resume aligns with the job description point, mark it with ✅ and provide a detailed explanation.  
+    - If the resume doesn't align with the job description point, mark it with ❌ and provide a reason for it.  
+    - If a clear conclusion cannot be made, use a ⚠️ sign with a reason.  
+    - The Final Heading should be "Suggestions to improve your resume:" and give where and what the candidate can improve to be selected for that job role.
+
+    # Inputs:
+    Candidate Resume: {resume}
+    ---
+    Job Description: {job_desc}
+
+    # Output:
+    - Each any every point should be given a score (example: 3/5 ). 
+    - Mention the scores and  relevant emoji at the beginning of each point and then explain the reason.
+    """
 
     chat_completion = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
@@ -75,6 +97,9 @@ def extract_scores(text):
     # Convert matches to floats
     scores = [float(match) for match in matches]
     return scores
+
+
+
 
 # <--------- Starting the Work Flow ---------> 
 
